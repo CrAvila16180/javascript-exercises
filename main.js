151bb8ef0f02eb85fdc9872eebd4b19ddbf63ae5
CR_Avila <carlosrobertoavilah@gmail.com>
2018-12-31 17:08:55 -0600

    Fix html function

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const log = (content) => console.log(content);
+const logIns = (content) => console.log(`%c ${content} `, 'background: #222; color: #bada55');
 
-const article = document.querySelector('article');
-const section = document.createElement('section');
-const para = document.createElement('p');
-const ins = document.createElement('p');
-const h2 = document.createElement('h2');
-const hr = document.createElement('hr');
-const div = document.createElement('div');
 
 const html = (number,name,instructions,code) => {
+    const article = document.querySelector('article');
+    const section = document.createElement('section');
+    const para = document.createElement('p');
+    const ins = document.createElement('p');
+    const h2 = document.createElement('h2');
+    const hr = document.createElement('hr');
+    const div = document.createElement('div');
+
+
     article.appendChild(section);
     section.appendChild(para);
     para.appendChild(h2);
@@ -24,18 +27,39 @@ const html = (number,name,instructions,code) => {
 
 const ex = (number,name) => {
     log('');
-    log(`Exercise ${number}`);
-    log(name);
+    logIns(`Exercise ${number}`);
+    logIns(name);
     log('');
 };
 
-
-
-html(1, 'Hello World', 'Write a function that returns "Hello, World!', "let helloWorld = () => 'Hello, World!';" )
 
 ex(1, 'Hello World');
 
 let helloWorld = () => 'Hello, World!';
 
 log(helloWorld());
+
+html(1, 'Hello World', 'Write a function that returns "Hello, World!', "let helloWorld = () => 'Hello, World!';" )
+
+ex(2, 'repeatString');
+
+let repeatString = (string, times) => {
+    let str = '';
+    for(i = 0; i < times; i ++){
+        str += string;
+    } if ( times < 0){
+        return 'ERROR'
+    } else {
+    return str;
+    }
+};
+
+log(repeatString('hey',3));
+log(repeatString('hey',10));
+log(repeatString('hey',1));
+log(repeatString('hey',0));
+log(repeatString('hey',-1));
+
+
+
 
