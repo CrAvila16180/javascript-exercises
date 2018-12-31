5f7cb71a6f02ec2015082121764685f1e2dea562
CR_Avila <carlosrobertoavilah@gmail.com>
2018-12-31 16:41:00 -0600

    Create html and log functions

---
+++ main.js
@@ -0,0 +1,40 @@
+const log = (content) => console.log(content);
+
+const article = document.querySelector('article');
+const section = document.createElement('section');
+const para = document.createElement('p');
+const ins = document.createElement('p');
+const h2 = document.createElement('h2');
+const hr = document.createElement('hr');
+const div = document.createElement('div');
+
+const html = (number,name,instructions,code) => {
+    article.appendChild(section);
+    section.appendChild(para);
+    para.appendChild(h2);
+    h2.textContent = `Exercise ${number} - ${name}`;
+    section.appendChild(hr);
+    section.appendChild(ins);
+    ins.textContent = instructions;
+    section.appendChild(div);
+    div.classList.add('code');
+    div.textContent = code
+}
+
+html(1, 'Hello World', 'Write a function that returns "Hello, World!', "let helloWorld = () => 'Hello, World!';" )
+
+const ex = (number,name) => {
+    log('');
+    log(`Exercise ${number}`);
+    log(name);
+    log('');
+};
+
+
+
+
+ex(1, 'Hello World')
+
+let helloWorld = () => 'Hello, World!';
+
+log(helloWorld())
